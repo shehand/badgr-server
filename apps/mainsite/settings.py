e0512d0f9efedbe10e154601ab4b2aea3eb62a15
Shehan Dhaleesha <sonodewcomplex@gmail.com>
2020-03-05 13:59:00 +0530

    Update settings.py

--- apps/mainsite/settings.py
+++ apps/mainsite/settings.py
@@ -122,7 +122,7 @@ TEMPLATES = [
 #
 ##
 
-HTTP_ORIGIN = "http://35.192.136.157:8000"
+HTTP_ORIGIN = "http://34.67.163.193/:8000"
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
